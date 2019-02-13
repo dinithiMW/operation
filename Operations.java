174af0e1cdba70fe1179d909d29e51bb125d9399
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-02-13 18:36:25 +0530

    operation
    operation

---
+++ Operations.java
@@ -0,0 +1,16 @@
+
+public class Operations {
+    
+    public static void main(String args[]){
+    
+        int var1=10;
+        int var2=5;
+        
+        System.out.println("Addition is :"+(var1+var2));
+        System.out.println("subtractin is :"+(var1-var2));
+        System.out.println("multiplication is :"+(var1*var2));
+        System.out.println("modulus is :"+(var1%var2));
+    
+    }
+    
+}
